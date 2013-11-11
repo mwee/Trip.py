778de2e3518a34a63833b93f462fe47a3a26c156
Faith Keza <fkeza@mit.edu>
2013-11-11 15:10:48 -0500

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LyxhLiyihuaFkezaMweeFinal::Application.config.secret_key_base = '25bc73867da4a2381579a39148a3c6a6a36398ae15957d8516a71af1c6cebfad8d78f804b90e840d4d2dacd46dfb978c942f5617a399f3ad5f064cfdf84ea45f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LyxhLiyihuaFkezaMweeFinal::Application.config.session_store :cookie_store, key: '_lyxh_liyihua_fkeza_mwee_final_session'
