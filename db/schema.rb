2805cba0b1b8bf961e61cf93f4720902a33cdc93
liyihua <liyihua@mit.edu>
2013-11-16 13:57:51 -0500

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116001505) do
-
+ActiveRecord::Schema.define(version: 20131116184352) do
   create_table "dateranges", force: true do |t|
     t.date     "start_date"
     t.date     "end_date"
