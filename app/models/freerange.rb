457ee95c55a586515d7d2be1fd5adfb20a88e50d
liyihua <liyihua@mit.edu>
2013-11-18 00:52:08 -0500

    Show friends working now

--- app/models/freerange.rb
+++ app/models/freerange.rb
@@ -2,4 +2,8 @@ class Freerange < ActiveRecord::Base
 	belongs_to :user
 	validates_date :start_date, :on_or_after => Time.now
 	validates_date :end_date, :on_or_after => :start_date
+	
+	def owns_range(user)
+    	return self.user.id==user.id
+	end
 end
