850ff7b951e69b7cffc53129c9a86f8111f37bcb
liyihua <liyihua@mit.edu>
2013-12-08 14:59:30 -0500

    merge

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -1,55 +1,57 @@
 $(document).ready(function() {
-    if (window.location.hash && window.location.hash == '#_=_') {
-        if (window.history && history.pushState) {
-            window.history.pushState("", document.title, window.location.pathname);
-        } else {
-            // Prevent scrolling by storing the page's current scroll offset
-            var scroll = {
-                top: document.body.scrollTop,
-                left: document.body.scrollLeft
-            };
-            window.location.hash = '';
-            // Restore the scroll offset, should be flicker free
-            document.body.scrollTop = scroll.top;
-            document.body.scrollLeft = scroll.left;
-        }
-    }
-    
+	//remove hash after facebook redirect
+	if (window.location.hash && window.location.hash == '#_=_') {
+		if (window.history && history.pushState) {
+			window.history.pushState("", document.title, window.location.pathname);
+		} else {
+			// Prevent scrolling by storing the page's current scroll offset
+			var scroll = {
+				top : document.body.scrollTop,
+				left : document.body.scrollLeft
+			};
+			window.location.hash = '';
+			// Restore the scroll offset, should be flicker free
+			document.body.scrollTop = scroll.top;
+			document.body.scrollLeft = scroll.left;
+		}
+	}
+
+	//initialize facebook sdk
 	window.fbAsyncInit = function() {
 		FB.init({
 			appId : '1375015546079605', // App ID
 			status : false, // check login status
 			cookie : true, // enable cookies to allow the server to access the session
 			xfbml : true, // parse XFBML
-			channelUrl   : 'https://desolate-everglades-8674.herokuapp.com/home/fbchannel',
-			oauth: true
+			channelUrl : 'https://desolate-everglades-8674.herokuapp.com/home/fbchannel',
+			oauth : true
 		});
 
-	};
-	
-	
-  (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
-
+	}; ( function(d) {
+			var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+			if (d.getElementById(id)) {
+				return;
+			}
+			js = d.createElement('script');
+			js.id = id;
+			js.async = true;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			ref.parentNode.insertBefore(js, ref);
+		}(document));
 
 	$("#invite").click(function() {
-		
-	
 		FB.ui({
 			method : 'apprequests',
 			message : 'I am already using Trippy. Please join me.'
 		}, function(response) {
 			alert("finished");
-			
+
 			var request = response.request;
 			var from = $("#user_data").attr("data_user_id");
 			$.ajaxSetup({
-				headers : {	'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')}
+				headers : {
+					'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
+				}
 			});
 			for (var i = 0; i < response.to.length; i++) {
 				var to = response.to[i];
@@ -64,43 +66,30 @@ $(document).ready(function() {
 						user_id : from,
 						friend_uid : to,
 					},
-				success : function() {
+					success : function() {
 						console.log("Data Send!");
 					},
 					error : function(xhr) {
 						console.log("The error code is: " + xhr.statusText);
 					}
 				});
-
 			}
-			
 		});
-		
 	});
-
 
 	$("#sign_out").click(function() {
 		FB.getLoginStatus(function(response) {
 			if (response.authResponse) {
-				// the user is logged in and has authenticated your
-				// app, and response.authResponse supplies
-				// the user's ID, a valid access token, a signed
-				// request, and the time the access token
-				// and signed request each expire
-				//var uid = response.authResponse.userID;
-				//var accessToken = response.authResponse.accessToken;
+				// the user is logged in and has authenticated the app
 				FB.logout();
 			}
 		});
 
 	});
-	
-	
-		$(function() {
+
+	//allow app to break out of facebook canvas
+	$(function() {
 		if (top.location != self.location) {
-			console.log("change top location");
-			console.log(self.location);
-			console.log(top.location);
 			top.location = self.location;
 		}
 	});
