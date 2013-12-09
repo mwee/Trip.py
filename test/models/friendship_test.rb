cba04c0afa6bdc49e3631562f508dcd35e246202
liyihua <liyihua@mit.edu>
2013-12-08 22:39:11 -0500

    finish friendship test

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class FriendshipTest < ActiveSupport::TestCase
   def test_is_friend
     assert(Friendship.is_friend?(1, 2))
-    assert_not(Friendship.is_friend?(1, 3))
+    assert_not(Friendship.is_friend?(1, 4))
   end
 
   def test_is_friend_self
@@ -33,18 +33,39 @@ class FriendshipTest < ActiveSupport::TestCase
     assert_equal(friendships.first.status,"pending")
     assert_equal(friendships.first.user_id,1)
     assert_equal(friendships.last.status,"pending")
-    assert_equal(friendships.last.user_id,4)   
-  end  
- 
-   def test_update
-      friendship=friendships(:one) 
-      friendship.update 
-      assert_equal(friendship.status,"finalized")
-  end  
-  
+    assert_equal(friendships.last.user_id,4)
+  end
+
+  def test_update
+    friendship=friendships(:one)
+    friendship.update
+    assert_equal(friendship.status,"finalized")
+  end
+
   def test_update_finalized
-      friendship=friendships(:two) 
-      friendship.update 
-      assert_equal(friendship.status,"finalized")
-  end  
+    friendship=friendships(:two)
+    friendship.update
+    assert_equal(friendship.status,"finalized")
+  end
+
+  def test_createInverse
+      friendship=friendships(:two)
+      friendship.createInverse
+      new_friendship=Friendship.where(user_id:3, friend_id:2,status:"finalized")
+      assert_not_nil(new_friendship)
+      
+  end
+
+  def test_createInverse_already_exist
+      friendship=friendships(:seven)  
+      friendship.createInverse
+      new_friendship=Friendship.where(user_id:3, friend_id:1,status:"finalized")
+      assert_not_nil(new_friendship)
+  end
+
+  def test_updateId
+    Friendship.updateId('100001837462039', 4)
+    friendship=friendships(:six)
+    assert_equal(friendship.friend_id,4)
+  end
 end
