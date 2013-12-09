c93e143124ca9ad3edebb16f74cf099da243b693
liyihua <liyihua@mit.edu>
2013-12-08 21:13:12 -0500

    delete fb init

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -16,28 +16,6 @@ $(document).ready(function() {
 		}
 	}
 
-	//initialize facebook sdk
-	window.fbAsyncInit = function() {
-		FB.init({
-			appId : '1375015546079605', // App ID
-			status : false, // check login status
-			cookie : true, // enable cookies to allow the server to access the session
-			xfbml : true, // parse XFBML
-			channelUrl : 'https://infinite-beyond-7744.herokuapp.com/home/fbchannel',
-			oauth : true,
-		});
-
-	}; ( function(d) {
-			var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-			if (d.getElementById(id)) {
-				return;
-			}
-			js = d.createElement('script');
-			js.id = id;
-			js.async = true;
-			js.src = "//connect.facebook.net/en_US/all.js";
-			ref.parentNode.insertBefore(js, ref);
-		}(document));
 
 	$("#invite").click(function() {
 		FB.ui({
